a6ef335d28da0928ab56af5fed5f1080f0435b0b
kkonii <119216472+kkonii@users.noreply.github.com>
2025-10-29 23:20:25 +0900

    test: LottoError 클래스의 예외 문구 반환 형식을 테스트

---
+++ src/test/java/lotto/exception/LottoErrorTest.java
@@ -0,0 +1,18 @@
+package lotto.exception;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LottoErrorTest {
+
+    @Test
+    void 예외_객체는_문구_앞에_헤더가_붙은_문자열을_리턴한다() {
+        //given
+        LottoError error = LottoError.NUMBERS_ARE_NOT_UNIQUE;
+        //when
+        String exceptionMessage = error.message();
+        //then
+        Assertions.assertThat(exceptionMessage)
+                .isEqualTo("[ERROR] 로또 번호는 중복되지 않아야 합니다.");
+    }
+}
