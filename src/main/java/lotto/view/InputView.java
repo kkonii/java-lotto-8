5bb390bea547ccda05403ac012651056386868ce
kkonii <119216472+kkonii@users.noreply.github.com>
2025-11-02 23:30:56 +0900

    feat: 콘솔로부터 사용자 입력값을 요청하여 반환

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,28 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String ENTER_PURCHASE_AMOUNT = "구입금액을 입력해 주세요.";
+    private static final String ENTER_WINNING_NUMBER = "당첨 번호를 입력해 주세요.";
+    private static final String ENTER_BONUS_NUMBER = "보너스 번호를 입력해 주세요.";
+
+    public String getAmountInput() {
+        System.out.println(ENTER_PURCHASE_AMOUNT);
+
+        return Console.readLine();
+    }
+
+    public String getWinningNumberInput() {
+        System.out.println(ENTER_WINNING_NUMBER);
+
+        return Console.readLine();
+    }
+
+    public String getBonusInput() {
+        System.out.println(ENTER_BONUS_NUMBER);
+
+        return Console.readLine();
+    }
+}
