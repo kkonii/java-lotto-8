07f639ecb62f541ecc8d6a7fde47c8380a1245f2
kkonii <119216472+kkonii@users.noreply.github.com>
2025-11-03 02:48:44 +0900

    refactor: 에러 객체에 누락된 헤더를 함께 생성하도록 변경

--- src/main/java/lotto/exception/GlobalError.java
+++ src/main/java/lotto/exception/GlobalError.java
@@ -5,10 +5,12 @@ public enum GlobalError {
     NOT_A_NUMBER("숫자 값만 입력할 수 있습니다."),
     OUT_OF_INTEGER_RANGE("숫자의 입력값이 너무 큽니다. 처리 가능한 범위를 초과했습니다.");
 
+    private static final String HEADER = "[ERROR] ";
+
     private final String message;
 
     GlobalError(String message) {
-        this.message = message;
+        this.message = HEADER.concat(message);
     }
 
     public String message() {
