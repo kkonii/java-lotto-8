61649c4ee0245d031c0a1f7fe4f0ec5ae63c287b
kkonii <119216472+kkonii@users.noreply.github.com>
2025-11-03 14:52:18 +0900

    feat: 총 수익률을 둘째 자리에서 반올림하여 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -6,6 +6,7 @@ import lotto.dto.LottoDto;
 public class OutputView {
 
     private static final String PURCHASE_AMOUNT_IS = "%d개를 구매했습니다.";
+    private static final String PROFIT_RATE_IS = "총 수익률은 %.1f%%입니다.";
 
     public void printPurchasedLottos(int amount, List<LottoDto> lottos) {
         System.out.printf(PURCHASE_AMOUNT_IS, amount);
@@ -13,5 +14,9 @@ public class OutputView {
         for (LottoDto lotto : lottos) {
             System.out.println(lotto.numbers());
         }
+    }
+
+    public void printProfitRate(float value) {
+        System.out.printf(PROFIT_RATE_IS, value);
     }
 }
